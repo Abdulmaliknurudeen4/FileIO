1a5478c83a25c329cb06b0f138bf825d2791a1ec
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-12-28 16:43:34 +0100

    Add BufferedOutput Stream Code of Reading into Using ByteArrays

--- src/com/example/vaos/Main.java
+++ src/com/example/vaos/Main.java
@@ -1,27 +1,25 @@
 package com.example.vaos;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         Main main = new Main();
-//        main.readFile();
-        main.readFileIntoBytes();
+        main.writeFileBufferred();
+
     }
 
     private void readFile() throws IOException {
         long startTime = System.nanoTime();
         FileInputStream fileInputStream = null;
         BufferedInputStream bufferedInputStream = null;
-        try{
+        try {
             fileInputStream = new FileInputStream("myinputfile.txt");
-            bufferedInputStream = new BufferedInputStream(fileInputStream, (8*1024));
-            int i ;
-            while ((i = bufferedInputStream.read()) != -1){
+            bufferedInputStream = new BufferedInputStream(fileInputStream, (8 * 1024));
+            int i;
+            while ((i = bufferedInputStream.read()) != -1) {
                 //  so as long the Buffer isn't emtpy
 //                System.out.print((char)i);
             }
@@ -29,17 +27,18 @@ public class Main {
             System.out.printf("Time to decode %,d MB was %,d ms%n",
                     fileInputStream.available() / 1024 / 1024, time / 1000000);
         } finally {
-            if(fileInputStream!=null)
+            if (fileInputStream != null)
                 fileInputStream.close();
-            if(bufferedInputStream!=null)
+            if (bufferedInputStream != null)
                 bufferedInputStream.close();
         }
 
     }
-    private void readFileIntoBytes() throws IOException{
+
+    private void readFileIntoBytes() throws IOException {
         FileInputStream fileInputStream = null;
         BufferedInputStream bufferedInputStream = null;
-        try{
+        try {
             fileInputStream = new FileInputStream("myinputfile.txt");
             bufferedInputStream = new BufferedInputStream(fileInputStream);
             //  Sets the destination bytes where the bufferedInputStream read data into
@@ -48,14 +47,36 @@ public class Main {
             // Read specified amount of bytes with offest beginnning at zero and 11 bytes
             int numberOfBytesread = bufferedInputStream.read(destBuffer, 0, 11);
             for (byte b : destBuffer) {
-                System.out.print((char)b);
+                System.out.print((char) b);
             }
 
-    }finally {
-            if(fileInputStream!=null)
+        } finally {
+            if (fileInputStream != null)
                 fileInputStream.close();
-            if(bufferedInputStream!=null)
+            if (bufferedInputStream != null)
                 bufferedInputStream.close();
         }
+    }
+
+    private void writeFileBufferred() throws IOException {
+        FileOutputStream fileOutputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        try{
+            fileOutputStream = new FileOutputStream("output.txt");
+            bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+            String str = "Peter is going to India.";
+            byte[] strArray = str.getBytes();
+
+            bufferedOutputStream.write(strArray);
+            bufferedOutputStream.flush();
+            System.out.println("SuccessFully written to the file" +
+                    "Please check the file for confirmation.");
+        }finally {
+            if (fileOutputStream != null)
+                fileOutputStream.close();
+            if (bufferedOutputStream != null)
+                bufferedOutputStream.close();
         }
+    }
 }
